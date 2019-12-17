20789fbf23524d8d38a371147c56e41969ce18aa
jinah <jinah0818@gmail.com>
2019-12-17 21:57:38 +0900

    Commit the simple code

--- programmers/Practice/2016.py
+++ programmers/Practice/2016.py
@@ -1,11 +1,8 @@
 import datetime
-
+import time
 weekday = ['SUN','MON','TUE','WED','THU','FRI','SAT']
 
 def solution(a, b):
     day = datetime.date(2016, a, b)
-    week = day.weekday()
+    week = day.weekday() + 1
     return weekday[week]
-
-TEST = solution(1, 1)
-print(TEST)
