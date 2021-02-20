769f5361fe0fa5942d984e95e37b447d9fc1e27d
jinah <jinah0818@gmail.com>
2021-02-20 12:54:18 +0900

    DP_memo bottom_up방식으로 변경

--- code-it/5.Dynamic Programming/fibo_memo.py
+++ code-it/5.Dynamic Programming/fibo_memo.py
@@ -5,18 +5,17 @@ def fib_memo(n, cache):
     if cache[n] == 1 or cache[n] == 2:
         return 1
     
-    return cache[n-2] + cache[n-1]
+    if n in cache:
+        return cache[n]
+    
+    cache[n] = fib_memo(n-2, cache) + fib_memo(n-1, cache)
+    
+    return cache[n]
 
 
 def fib(n):
     # n번째 피보나치 수를 담는 사전
     fib_cache = {}
-
-    for idx in range(1, n+1):
-        if idx == 1 or idx == 2:
-            fib_cache[idx] = 1
-            continue
-        fib_cache[idx] = fib_cache[idx-2]+fib_cache[idx-1]
 
     return fib_memo(n, fib_cache)
 
