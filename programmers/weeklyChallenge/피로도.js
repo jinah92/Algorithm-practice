6e1f055fa7c60e9697ce935b0fdf067afb5a6d96
jinah92 <jinah0818@gmail.com>
2022-02-20 01:24:22 +0900

    lv2. 피로도

--- programmers/weeklyChallenge/피로도.js
+++ programmers/weeklyChallenge/피로도.js
@@ -1,46 +1,24 @@
+let max = 0;
+let visit;
+
 function solution(k, dungeons) {
-  let answer = 0;
-  let obj = {};
-
-  let needs = [];
-
-  dungeons.forEach((hp) => {
-    needs.push(hp[0]);
-    obj[hp[0]] = [hp[1], 0];
-  });
-
-  needs.sort((a, b) => b - a);
-
-  let _k = k;
-  let _list = [needs[0]];
-  let tmp = [needs[0], needs[0] - obj[needs[0]][0]];
-  let i = 0;
-  _k = _k - obj[needs[0]][0];
-  // 첫번째로 필요 피로도가 높은곳 을 방문
-  while (i < needs.length) {
-    needs.forEach((el) => {
-      if (obj[el][0] - _k && !_list.includes(el)) {
-        if (tmp[1] > _k - obj[el][0]) {
-          tmp = [el, _k - obj[el][0]];
-        }
-      }
-    });
-    _list.push(tmp[0]);
-    i += 1;
+  visit = Array.from({ length: dungeons.length }, () => null);
+  for (let i = 0; i < dungeons.length; i++) {
+    if (k >= dungeons[i][0]) dfs(i, k, 1, dungeons);
   }
 
-  // 첫번째로 잔여 피로도가 높은 곳을 방문
+  return max;
+}
 
-  console.log(obj);
-
-  while (k) {
-    for (let key in obj) {
-      obj[key][1] = k - obj[key][0];
-    }
-    console.log(obj);
+function dfs(curr, tired, depth, dungeons) {
+  visit[curr] = true;
+  tired -= dungeons[curr][1];
+  for (let i = 0; i < dungeons.length; i++) {
+    if (!visit[i] && dungeons[i][0] <= tired)
+      dfs(i, tired, depth + 1, dungeons);
   }
-
-  return answer;
+  max = Math.max(depth, max);
+  visit[curr] = false;
 }
 
 solution(80, [
